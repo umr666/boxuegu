121d6f900ac02d6e1f00324d320a20dbae09c19e
gang <609369350@qq.com>
2017-04-03 17:34:54 +0800

    配置require

---
+++ js/main.js
@@ -0,0 +1,44 @@
+define([],function(){
+    require.config({
+        baseUrl:'./',
+        paths:{
+            //配置自己写的js模块别名
+            advertAdd:'js/advert/advert_add',
+            advertList:'js/advert/advert_list',
+            courseAdd1:'js/course/course_add_step1',
+            courseAdd1:'js/course/course_add_step2',
+            courseAdd1:'js/course/course_add_step3',
+            courseAdd:'js/course/course_add',
+            courseCategoryAdd:'js/course/course_category_add',
+            courseCategory:'js/course/course_category',
+            courseList:'js/course/course_list',
+            courseTopic:'js/course/course_topic',
+            login:'js/home/login',
+            repass:'js/home/repass',
+            settings:'js/home/settings',
+            teacherAdd:'js/teacher/teacher_add',
+            teacherList:'js/teacher/teacher_list',
+            userProfile:'js/user/user_profile',
+            userList:'js/user/user_list',
+
+            //配置第三方js模块别名
+            template:'lib/artTemplate/template-debug',
+            bootstrap:'lib/bootstrap/js/bootstrap',
+            datepicker:'lib/bootstrap-datepicker/js/bootstrap-datepicker',
+            ckeditor:'lib/ckeditor/ckeditor',
+            ckeditorLand:'lib/ckeditor/lang/zh-cn',
+            echarts:'lib/echarts/echarts.min',
+            jquery:'lib/jquery/jquery.min',
+            jqueryCookie:'lib/jquery-cookie/jquery.cookie',
+            jqueryForm:'lib/jquery-form/jquery.form',
+            jqueryRegion:'lib/jquery-region/jquery.region',
+            nprogress:'lib/nprogress/nprogress',
+        },
+
+        shim:{
+            bootstrap:{
+                deps:['jquery']
+            }
+        }
+    })
+})
