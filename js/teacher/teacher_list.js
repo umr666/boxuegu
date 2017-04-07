cea116d3954c994aa5a30286fd1c558ee98b9f33
gang <609369350@qq.com>
2017-04-07 10:13:57 +0800

    教师列表获取

--- js/teacher/teacher_list.js
+++ js/teacher/teacher_list.js
@@ -1,3 +1,15 @@
-define(['aside'], function() {
-	
+define(['aside', 'header', 'loading', 'nprogress', 'jquery', 'template'], function(undefined, undefined, undefined, nprogress, $, template) {
+    //请求教师列表
+	(function(){
+        $.get('/v6/teacher', function(data) {
+			if(data.code === 200) {
+                var result=template('template-teacher', data);
+				$('#table-teacher-list').append(result);
+			}
+
+            // nprogress.done();
+		});
+    })();
+
+    nprogress.done();
 });
--- js/user/user_list.js
+++ js/user/user_list.js
@@ -1,3 +1,16 @@
-define(['aside'], function() {
+define(['aside', 'header', 'loading', 'nprogress', 'jquery', 'template'], function(undefined, undefined, undefined, nprogress, $, template) {
 	
+    (function(){
+        $.get('/v6/course', function(data) {
+			if(data.code === 200) {
+                console.log(data);
+                // var result=template('template-teacher', data);
+				// $('#table-teacher-list').append(result);
+			}
+
+            // nprogress.done();
+		});
+    })();
+
+    nprogress.done();
 });
